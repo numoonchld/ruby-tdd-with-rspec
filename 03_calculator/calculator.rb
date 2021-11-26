1c82a086b341cc712c07592cb32c0a14efc860c1
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-26 15:02:55 +0530

    Arithmetic Calculator TDD

---
+++ 03_calculator/calculator.rb
@@ -0,0 +1,14 @@
+class Calculator
+  def self.sum(num_a, num_b)
+    num_a + num_b
+  end
+  def self.difference(num_a, num_b)
+    num_a - num_b
+  end
+  def self.product(num_a, num_b)
+    num_a * num_b
+  end
+  def self.quotient(num_a, num_b)
+    num_a.to_f / num_b
+  end
+end
---
+++ 03_calculator/calculator_spec.rb
@@ -0,0 +1,28 @@
+require_relative 'calculator'
+
+RSpec.describe Calculator do
+  context 'adding two numbers' do
+    it 'should return the sum of the two numbers' do
+      calc_sum = Calculator.sum(2,3)
+      expect(calc_sum).to eq(5)
+    end
+  end
+  context 'subtracting one number from another' do
+    it 'should return the difference' do
+      calc_difference = Calculator.difference(2,3)
+      expect(calc_difference).to eq(-1)
+    end
+  end
+  context 'multiplying two numbers' do
+    it 'should return the product of the two numbers' do
+      calc_product = Calculator.product(2,3)
+      expect(calc_product).to eq(6)
+    end
+  end
+  context 'diving one number by another' do
+    it 'should return the quotient' do
+      calc_quotient = Calculator.quotient(4,2)
+      expect(calc_quotient).to eq(2)
+    end
+  end
+end
