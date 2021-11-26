01d881b60e4d189eefcc94464905ad5879598f25
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-26 14:36:25 +0530

    Passing student tests

--- 02_student/student.rb
+++ 02_student/student.rb
@@ -2,6 +2,17 @@ class Student
   def initialize(first,last)
     @first_name = first
     @last_name = last
-    @full_name = first + ' ' + last
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def full_name
+    `#{@first_name} #{@last_name}`
   end
 end
