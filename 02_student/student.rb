9ddd4b57f67deb2797565cce82c8ae0aae6862f6
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-26 14:37:10 +0530

    Passing student tests with attribute reader

--- 02_student/student.rb
+++ 02_student/student.rb
@@ -1,15 +1,9 @@
 class Student
+  attr_reader :first_name, :last_name
+
   def initialize(first,last)
     @first_name = first
     @last_name = last
-  end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
   end
 
   def full_name
