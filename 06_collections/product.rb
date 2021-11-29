e59e3d6151a84e254525c385d983762d3fdb30ec
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-29 15:53:25 +0530

    Added functionality to retrieve all products created

--- 06_collections/product.rb
+++ 06_collections/product.rb
@@ -1,11 +1,17 @@
 class Product
   attr_reader :id, :name, :quantity, :price
+  @@products = []
 
   def initialize(data={})
     @id = data[:id] || 0
     @name = data[:name] || 'Test Product'
     @quantity = data[:quantity] || 0
     @price = data[:price] || 0
+    @@products << self
+  end
+
+  def self.all
+    @@products
   end
 
 end
--- 06_collections/product_spec.rb
+++ 06_collections/product_spec.rb
@@ -2,7 +2,7 @@ require_relative 'product'
 
 RSpec.describe Product do
   
-  before do
+  before(:all) do
     @product_a = Product.new({id: 1, name: 'Item1', quantity: 3, price: 25})
   end
 
@@ -27,6 +27,12 @@ RSpec.describe Product do
       expect(@product_a.name).to eq 'Item1'
       expect(@product_a.quantity).to eq 3
       expect(@product_a.price).to eq 25
+    end
+  end
+
+  context 'gets list of all products created' do
+    it 'returns an array of all products' do 
+      expect(Product.all).to eq [@product_a]
     end
   end
 end
