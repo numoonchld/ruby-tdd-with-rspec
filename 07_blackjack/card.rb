df21d8275c725531f2a9752b07f5fdf068337ad1
numoonchld <raghavendra.saralaya@gmail.com>
2021-11-30 17:37:11 +0530

    Basic Card class implementation

---
+++ 07_blackjack/card.rb
@@ -0,0 +1,19 @@
+class Card
+  attr_accessor :suit, :rank, :show
+  SUITS = ['Spades','Hearts', 'Clubs', 'Diamonds']
+  RANKS = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King', 'Ace']
+
+  def initialize(suit,rank)
+    @show = true
+    
+    if SUITS.include?(suit) && RANKS.include?(rank)
+      @suit = suit
+      @rank = rank
+    else
+      @suit = 'UNK'
+      @rank = 'UNK'
+    end
+  end
+
+
+end
---
+++ 07_blackjack/card_spec.rb
@@ -0,0 +1,31 @@
+require_relative 'card'
+
+RSpec.describe Card do
+  before do
+    suit = 'Diamonds'
+    rank = '8'
+
+    @card = Card.new(suit, rank)
+  end
+
+  it 'responds to suit' do
+    expect(@card).to respond_to(:suit)
+  end
+  it 'responds to rank' do
+    expect(@card).to respond_to(:rank)
+  end
+  it 'responds to show' do
+    expect(@card).to respond_to(:show)
+  end
+
+  it "'show' method returns 'true'" do
+    expect(@card.show).to eq(true)
+  end
+  it "'suit' method returns 'Diamonds'" do
+    expect(@card.suit).to eq('Diamonds')
+  end
+  it "'suit' method returns '8'" do
+    expect(@card.rank).to eq('8')
+  end
+
+end
