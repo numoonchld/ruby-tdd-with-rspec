5b0339530fbc328cb4b223c82304e683d8deda66
numoonchld <raghavendra.saralaya@gmail.com>
2021-12-06 18:15:06 +0530

    Buildind hand class with spec continued

--- 07_blackjack/hand.rb
+++ 07_blackjack/hand.rb
@@ -1,4 +1,20 @@
 class Hand
+
+  VALUES = {
+   '2':2,
+   '3':3,
+   '4':4,
+   '5':5,
+   '6':6,
+   '7':7,
+   '8':8,
+   '9':9,
+   'Jack':10,
+   'Queen':10,
+   'King':10,
+   'Ace':1,
+  }
+
   attr_accessor :dealt_cards
   
   def initialize
@@ -7,5 +23,17 @@ class Hand
 
   def add_card(card)
     dealt_cards.push(card)
+  end
+
+  def get_value
+    card_ranks = []
+    dealt_cards.each { |card| card_ranks.push(card.rank)}
+    
+    value = 0 
+    card_ranks.each do |rank|
+      value = value + VALUES[rank.to_sym]
+    end
+
+    value
   end
 end
--- 07_blackjack/hand_spec.rb
+++ 07_blackjack/hand_spec.rb
@@ -16,17 +16,35 @@ RSpec.describe Hand do
       expect(@hand).to respond_to :add_card
       
     end
-    it 'returns the correct number of cards added' do
-      
+    it 'returns the correct cards added' do
+      card_a = Card.new('Diamonds','Jack')
+      card_b = Card.new('Clubs','Ace')      
+
+      @hand.add_card card_a
+      @hand.add_card card_b
+
+      expect(@hand.dealt_cards.size).to eq 2
+
+      expect(@hand.dealt_cards.first.suit).to eq 'Diamonds'
+      expect(@hand.dealt_cards.last.suit).to eq 'Clubs'
+
+      expect(@hand.dealt_cards.first.rank).to eq 'Jack'
+      expect(@hand.dealt_cards.last.rank).to eq 'Ace'
     end
   end
 
   describe '#get_value' do
     it 'responds to get_value' do
-      
+      expect(@hand).to respond_to :get_value
     end
     it 'returns correct value without Ace' do
+      card_a = Card.new('Diamonds','Jack')
+      card_b = Card.new('Clubs','9')      
       
+      @hand.add_card card_a
+      @hand.add_card card_b
+
+      expect(@hand.get_value).to eq 10 + 9
     end
     it 'returns correct value with an Ace and a Jack' do
       
