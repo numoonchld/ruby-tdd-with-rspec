7ec56f33627a9c99d5cd5988b166ffa9e04cfb2b
numoonchld <raghavendra.saralaya@gmail.com>
2021-12-08 20:27:08 +0530

    Fixed hand.rb logic

--- 07_blackjack/hand.rb
+++ 07_blackjack/hand.rb
@@ -10,11 +10,12 @@ class Hand
     '7':7,
     '8':8,
     '9':9,
+    '10':10,
     'Jack':10,
     'Queen':10,
     'King':10,
   }
-
+  
   attr_accessor :dealt_cards
   
   def initialize
@@ -22,23 +23,30 @@ class Hand
   end
 
   def add_card(card)
-    dealt_cards.push(card)
+    @dealt_cards.push(card)
   end
 
   def get_value
-    card_ranks = dealt_cards.map(&:rank)
-    # card_ranks = dealt_cards.map { |card| card.rank}
+    # card_ranks = @dealt_cards.map(&:rank)
+    card_ranks = @dealt_cards.map { |card| card.rank }
     # card_ranks = dealt_cards.collect { |card| card.rank} # same as map
-    if card_ranks.include?('Ace')
-      card_ranks.reduce(0) { |acc,val| acc + VALUES[val.to_sym] } + 10
-    else
-      card_ranks.reduce(0) { |acc,val| acc + VALUES[val.to_sym] }
-    end
+
+    # if card_ranks.include?('Ace')
+    #   card_ranks.reduce(0) { |acc,val| acc + VALUES[val.to_sym] } + 10
+    # else
+    #   card_ranks.reduce(0) { |acc,val| acc + VALUES[val.to_sym].to_i }
+    # end
+    
     # value = 0 
     # card_ranks.each do |rank|
     #   value = value + VALUES[rank.to_sym]
     # end
-    # value
+    # puts card_ranks
+    value = card_ranks.reduce(0) { |acc,rank| acc + VALUES[rank.to_sym]}
+    if card_ranks.include?('Ace')
+      value += 10 if value + 10 <= 21
+    end
+    value
   end
 
   def to_s
